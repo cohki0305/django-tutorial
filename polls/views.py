d5984e4afe90d0995a17e845db126217931f647f
cohki0305 <cohki0305@gmail.com>
2017-12-21 10:55:58 +0900

    View を作成, Url を設定

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http      import HttpResponse # デフォルトで読み込まれないの？
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
